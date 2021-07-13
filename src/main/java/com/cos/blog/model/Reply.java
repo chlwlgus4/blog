0312bbc6a57d37c7d2fa639415908b1a9079afb9
chlwlgus4 <chlwlgus89@naver.com>
2021-07-14 08:11:45 +0900

    테이블 추가

--- src/main/java/com/cos/blog/model/Reply.java
+++ src/main/java/com/cos/blog/model/Reply.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
@@ -16,23 +14,22 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @Builder // 빌더 패턴
 @Entity
-public class User {
+public class Reply {
 
     @Id // 기본키
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
     private Long id;
 
-    @Column(nullable = false, length = 30)
-    private String username;
+    @Column(nullable = false, length = 200)
+    private String content;
 
-    @Column(nullable = false, length = 100)
-    private String password;
+    @ManyToOne
+    @JoinColumn(name="boardId")
+    private Board board;
 
-    @Column(nullable = false, length = 50)
-    private String email; //myEmail, my_email
-
-    @ColumnDefault("'user'")
-    private String role; // Enum을 쓰는게 좋다.
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
 
     @CreationTimestamp
     private Timestamp createDate;
