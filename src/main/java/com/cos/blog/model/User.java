4e90b563ed6145ca7882efb11eb7f24687f6cf02
chlwlgus4 <chlwlgus89@naver.com>
2021-07-19 07:54:21 +0900

    테이블 추가

--- src/main/java/com/cos/blog/model/User.java
+++ src/main/java/com/cos/blog/model/User.java
@@ -6,6 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.DynamicInsert;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
@@ -16,6 +17,7 @@ import java.sql.Timestamp;
 @AllArgsConstructor
 @Builder // 빌더 패턴
 @Entity
+//@DynamicInsert insert시에 null인 필드를 제외
 public class User {
 
     @Id // 기본키
@@ -31,8 +33,9 @@ public class User {
     @Column(nullable = false, length = 50)
     private String email; //myEmail, my_email
 
-    @ColumnDefault("'user'")
-    private String role; // Enum을 쓰는게 좋다.
+//    @ColumnDefault("'user'")
+    @Enumerated(EnumType.STRING)
+    private RoleType role; // Enum을 쓰는게 좋다.
 
     @CreationTimestamp
     private Timestamp createDate;
---
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -0,0 +1,61 @@
+package com.cos.blog.test;
+
+import com.cos.blog.model.RoleType;
+import com.cos.blog.model.User;
+import com.cos.blog.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@RestController
+public class DummyControllerTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        System.out.println("user = " + user.toString());
+
+        User updateUser = userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("수정에 실패하였습니다."));
+        updateUser.setEmail(user.getEmail());
+        updateUser.setPassword(user.getPassword());
+
+//        userRepository.save(updateUser);
+
+//        더티 체킹
+        return null;
+    }
+
+    @GetMapping("/dummy/user")
+    public List<User> list() {
+        return userRepository.findAll();
+    }
+
+    //페이징
+    @GetMapping("/dummy/user/page")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return userRepository.findAll(pageable).getContent();
+
+    }
+
+    @GetMapping("/dummy/user/{id}")
+    public User Detail(@PathVariable Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다. id = " + id));
+    }
+
+    @PostMapping("/dummy/join")
+    public String join(User user) {
+        System.out.println("user = " + user.toString());
+        user.setRole(RoleType.USER);
+        userRepository.save(user);
+        return "회원가입이 완료되었습니다.";
+    }
+}
